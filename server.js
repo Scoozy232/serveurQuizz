41dfd115010655b501a263b612de5cfd0768ae76
scoozy23 <scoozy23@gmail.com>
2022-11-10 13:16:56 +0100

    fff

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.listen(3001, () => {
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://EmmaLoisel:toucan@cluster0.y10v2kc.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'quizzApi';
-const client = new MongoClient(url)
 let db
-MongoClient.connect(client, function(err, client) {
+MongoClient.connect(url, function(err, client) {
   console.log("Connected successfully to server");
   db = client.db(dbName);
 });
