a77f4ed9b9fe700b951699bf7daa99addb755f3b
Scoozy232 <39296298+Scoozy232@users.noreply.github.com>
2022-11-15 11:31:40 +0100

    cors

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.listen(port, () => {
   console.log("Server is running on port 3001");
 });
  
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 
